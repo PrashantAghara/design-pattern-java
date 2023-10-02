21c1f2fb5e4d7763b8188b89638679f82b64e0ec
prashant <prashant.aghara2111@gmail.com>
2023-10-02 17:48:34 +0530

    Observer

---
+++ src/main/java/com/designpatterns/behavioral/observer/ObserverMain.java
@@ -0,0 +1,16 @@
+package com.designpatterns.behavioral.observer;
+
+public class ObserverMain {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+        new BinaryObserver(subject);
+        new OctalObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+        subject.notifyAllObservers();
+        System.out.println("Second state change: 10");
+        subject.setState(10);
+        subject.notifyAllObservers();
+    }
+}
