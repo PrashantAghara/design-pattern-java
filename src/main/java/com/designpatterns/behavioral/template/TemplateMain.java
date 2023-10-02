b02e7ce78655c55c4fbb5f86724f2d6d767fd641
prashant <prashant.aghara2111@gmail.com>
2023-10-02 18:13:57 +0530

    Template

---
+++ src/main/java/com/designpatterns/behavioral/template/TemplateMain.java
@@ -0,0 +1,8 @@
+package com.designpatterns.behavioral.template;
+
+public class TemplateMain {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+    }
+}
