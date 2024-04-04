b8648a4c1e7082f21cc8650a128c825d723216ba
Prashant Aghara <prashant.aghara2111@gmail.com>
2024-04-04 21:21:38 +0530

    Adapter changes

--- src/main/java/code/design/adaptor/AdapterMain.java
+++ src/main/java/code/design/adaptor/AdapterMain.java
@@ -3,8 +3,7 @@ package code.design.adaptor;
 public class AdapterMain {
     public static void main(String[] args) {
         AndroidCharger androidCharger = new ChargerAND();
-        ChargerAdapter adapter = new ChargerAdapter(androidCharger);
-
+        AppleCharger adapter = new ChargerAdapter(androidCharger);
         IPhone13 iPhone13 = new IPhone13(adapter);
         iPhone13.chargePhone();
     }
